6c63fbf166735eb28005236b2ac5ba89b3b3650d
shenjikang <shenjikang@bytedance.com>
2020-10-15 15:20:23 +0800

    add ssr site prerender

---
+++ site/exp/ssr/my-app/build/precache-manifest.cab0daaaf511a5447c4060022d466d78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb241075cc7fd7c63ee779324b3e1bd6",
+    "url": "index.html"
+  },
+  {
+    "revision": "771355aaf676586cfb3d",
+    "url": "static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a46617966b536919ab2",
+    "url": "static/js/2.68f56c50.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "static/js/2.68f56c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "771355aaf676586cfb3d",
+    "url": "static/js/main.ed0de294.chunk.js"
+  },
+  {
+    "revision": "f9a706d9f2b51bb78942",
+    "url": "static/js/runtime-main.32f3e913.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "static/media/logo.5d5d9eef.svg"
+  }
+]);
