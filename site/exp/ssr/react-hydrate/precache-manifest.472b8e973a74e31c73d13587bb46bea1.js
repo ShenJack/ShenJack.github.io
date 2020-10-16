b54e46af97065bff31166ef137fd70a029435a0f
shenjikang <shenjikang@bytedance.com>
2020-10-16 15:12:34 +0800

    add hydrate

---
+++ site/exp/ssr/react-hydrate/precache-manifest.472b8e973a74e31c73d13587bb46bea1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69dbe1dd6a9c98542648b273e78501eb",
+    "url": "index.html"
+  },
+  {
+    "revision": "a4d1b47022ae43ed203c",
+    "url": "static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a46617966b536919ab2",
+    "url": "static/js/2.68f56c50.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "static/js/2.68f56c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4d1b47022ae43ed203c",
+    "url": "static/js/main.abb742f3.chunk.js"
+  },
+  {
+    "revision": "f9a706d9f2b51bb78942",
+    "url": "static/js/runtime-main.32f3e913.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "static/media/logo.5d5d9eef.svg"
+  }
+]);
